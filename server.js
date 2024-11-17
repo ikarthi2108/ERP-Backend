a483ef3945109eb3c4b96c995e509e87f113ad8c
ikarthi2108 <karthicloud6@gmail.com>
2024-11-17 12:15:17 +0530

    second commit-17-11

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const formRoutes = require('./routes/formRoutes');
 
 // Load environment variables
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(cors());
 
 // Routes
 app.use('/api/users', userRoutes);
+app.use('/api/forms', formRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
